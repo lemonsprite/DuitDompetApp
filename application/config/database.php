f5fb0bd72f8baa0c62a0468cb1490b57e3b222d7
LemonSprite <lemonsprite@github.com>
2020-12-13 12:31:47 +0700

    Setting database awal

--- application/config/database.php
+++ application/config/database.php
@@ -70,15 +70,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
+$active_group = 'duitdompet';
 $query_builder = TRUE;
 
-$db['default'] = array(
+$db['duitdompet'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => '',
+	'username' => 'root',
 	'password' => '',
-	'database' => '',
+	'database' => 'duitdompet',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
